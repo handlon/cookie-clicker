d92dff903d0d4270e87cf4b7af909fad1e6d7cdc
Handlon <jan.handlik@gmail.com>
2023-09-19 15:36:54 +0200

    turned off user select

--- res/js/main.js
+++ res/js/main.js
@@ -1,11 +1,35 @@
 const susenka = document.getElementById("susenka");
-const count = document.getElementById("counter");
+const counter = document.getElementById("counter");
+const clickUpgrade = document.getElementById("clickuUpgrade")
 
 let numberOfCookies = 0;
+let costOfClickUpgrade = 20;
+let clickUpgradeIncrease = 1;
 
 susenka.onclick = () => {
     console.log("click");
-    numberOfCookies++;
+    numberOfCookies+= clickUpgradeIncrease;
     console.log(numberOfCookies);
     counter.innerHTML = "počet orea:" + numberOfCookies;
 }
+
+function cheats() {
+    console.log("cheats on!")
+    numberOfCookies += 100000000000;
+    counter.innerText = "počet orea:" + numberOfCookies;
+}
+
+
+const cheatsTwo = () => {
+    numberOfCookies *= 100000000000;
+   counter.innerText = "počet orea:" + numberOfCookies;
+}
+
+const clickUpgradeFunction = () => {
+    if (numberOfCookies >= costOfClickUpgrade) {
+        numberOfCookies -= costOfClickUpgrade;
+        counter.innerText = "počet orea:" + numberOfCookies;
+        clickUpgradeIncrease ++;
+    }
+}
+clickUpgrade.onclick = clickUpgradeFunction
