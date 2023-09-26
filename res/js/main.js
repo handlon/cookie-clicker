6a9e4d45e45671812538bf3de34b75b73e248fa2
Handlon <jan.handlik@gmail.com>
2023-09-26 14:36:21 +0200

    autoclicker upgrade

--- res/js/main.js
+++ res/js/main.js
@@ -1,10 +1,28 @@
 const susenka = document.getElementById("susenka");
 const counter = document.getElementById("counter");
 const clickUpgrade = document.getElementById("clickuUpgrade")
+const autoclicker = document.getElementById("autoclicker")
 
 let numberOfCookies = 0;
 let costOfClickUpgrade = 20;
 let clickUpgradeIncrease = 1;
+let autoClickIncrease = 0;
+let autoclick;
+
+
+autoclicker.onclick = () => {
+    if (numberOfCookies >= 100) {
+
+        numberOfCookies -= 100;
+        counter.innerText = "počet orea:" + numberOfCookies;
+        autoClickIncrease ++;
+        clearInterval(autoclick);
+        autoclick = setInterval( () => {
+            numberOfCookies += autoClickIncrease
+            counter.innerText = "počet orea:" + numberOfCookies;
+        },1000)
+    }
+}
 
 susenka.onclick = () => {
     console.log("click");
@@ -33,3 +51,7 @@ const clickUpgradeFunction = () => {
     }
 }
 clickUpgrade.onclick = clickUpgradeFunction
+
+cheats();
+
+
